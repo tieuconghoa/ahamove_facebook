58d43e13e6f9ba491f8a2e3a34fb30ae5fd6b0e3
juartaurus98 <kienhy1998@gmail.com>
2018-12-28 12:25:03 +0700

    second

--- server.js
+++ server.js
@@ -4,15 +4,42 @@ const request = require('request');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('group');
+  var url = `http://ahamove.cuongvc.com:8081/api/groups/get`
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('group', {Infor: null, error :"Error" });
+    } else {
+      const Inforg = JSON.parse(body)
+      if(Inforg.status != 'ok'){
+        res.render('group', {Infor: null, error :"Error" });
+      } else {
+        res.render('group', {Infor: Inforg, error :"Error" });
+      }
+    }
+  });
 })
-app.get('/post', function(req, res){
-  res.render('post');
+
+app.get('/post/:group_id', function(req, res){
+  var group_id = req.params['group_id'];
+  var url = `http://ahamove.cuongvc.com:8081/api/posts/get_by_group?group_id=${group_id}`
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('post', {Infor: null, error :"Error" });
+    } else {
+      const Inforp = JSON.parse(body)
+      if(Inforp.status != 'ok'){
+        res.render('post', {Infor: null, error :"Error" });
+      } else {
+        console.log(group_id);
+        res.render('post/:group_id', {Infor: Inforp, error :"Error" });
+      }
+    }
+  });
 })
 
 app.listen(8080, function () {
